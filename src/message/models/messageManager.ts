4453ff4301e978f841b20a0ddf58385ad81ef852
Asaf Masa <ASAFMAS@rafael.co.il>
2025-09-14 10:37:45 +0300

    chore: added mock post request

--- src/message/models/messageManager.ts
+++ src/message/models/messageManager.ts
@@ -3,21 +3,6 @@ import { inject, injectable } from 'tsyringe';
 import type { components } from '@openapi';
 import { SERVICES } from '@common/constants';
 
-const messageInstance: IMessageModel = {
-  sessionId: 9876543210,
-  severity: 'Error',
-  timeStamp: '2025-09-11T13:45:00.000Z',
-  message: 'Failed to authenticate user.',
-  messageParameters: {
-    userId: 'user123',
-    iP: '192.168.1.10',
-    attempt: '3',
-    reason: 'Invalid credentials',
-  },
-  component: 'AuthService',
-  messageType: 'Audit',
-};
-
 function generateRandomId(): number {
   const rangeOfIds = 100;
   return Math.floor(Math.random() * rangeOfIds);
