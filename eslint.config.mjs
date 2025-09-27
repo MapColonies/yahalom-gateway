74e680a8925f95a06b7868df4fa025501fa4c6cd
liran <lira.tulchin@gmail.com>
2025-09-27 22:58:32 +0300

    refactor: update get partial request

--- eslint.config.mjs
+++
@@ -1,5 +0,0 @@
-import tsBaseConfig from '@map-colonies/eslint-config/ts-base';
-import jestConfig from '@map-colonies/eslint-config/jest';
-import { config } from '@map-colonies/eslint-config/helpers';
-
-export default config(jestConfig, tsBaseConfig);
--- tests/configurations/initJestOpenapi.setup.ts
+++
@@ -1,4 +0,0 @@
-import path from 'path';
-import jestOpenApi from 'jest-openapi';
-
-jestOpenApi(path.join(process.cwd(), 'openapi3.yaml'));
