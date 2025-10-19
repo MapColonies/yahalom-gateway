2800df31d977d7ef27b983823aa3c5ca9779bedf
liran <lira.tulchin@gmail.com>
2025-10-19 19:25:30 +0300

    test: integration

--- src/DAL/migration/1760872317517-CreateTable.ts
+++ src/DAL/migration/1760872317517-CreateTable.ts
@@ -3,36 +3,28 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateTable1760872317517 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TYPE "public"."message_severity_enum" RENAME TO "message_severity_enum_old"`);
     await queryRunner.query(
       `CREATE TYPE "public"."message_severity_enum" AS ENUM('EMERGENCY', 'ALERT', 'CRITICAL', 'ERROR', 'WARNING', 'NOTICE', 'INFORMATIONAL', 'DEBUG')`
     );
-    await queryRunner.query(
-      `ALTER TABLE "message" ALTER COLUMN "severity" TYPE "public"."message_severity_enum" USING "severity"::"text"::"public"."message_severity_enum"`
-    );
-    await queryRunner.query(`DROP TYPE "public"."message_severity_enum_old"`);
-    await queryRunner.query(`ALTER TYPE "public"."message_component_enum" RENAME TO "message_component_enum_old"`);
+
     await queryRunner.query(`CREATE TYPE "public"."message_component_enum" AS ENUM('GENERAL', 'MAP', 'FTUE', 'SIMULATOR')`);
-    await queryRunner.query(
-      `ALTER TABLE "message" ALTER COLUMN "component" TYPE "public"."message_component_enum" USING "component"::"text"::"public"."message_component_enum"`
-    );
-    await queryRunner.query(`DROP TYPE "public"."message_component_enum_old"`);
+
+    await queryRunner.query(`
+      CREATE TABLE "message" (
+        "id" SERIAL PRIMARY KEY,
+        "sessionId" bigint NOT NULL,
+        "message" text NOT NULL,
+        "severity" "public"."message_severity_enum" NOT NULL,
+        "component" "public"."message_component_enum" NOT NULL,
+        "timeStamp" timestamp NOT NULL DEFAULT now()
+      )
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`CREATE TYPE "public"."message_component_enum_old" AS ENUM('GENERAL', 'MAP', 'FTUE', 'SIMULATOR')`);
-    await queryRunner.query(
-      `ALTER TABLE "message" ALTER COLUMN "component" TYPE "public"."message_component_enum_old" USING "component"::"text"::"public"."message_component_enum_old"`
-    );
-    await queryRunner.query(`DROP TYPE "public"."message_component_enum"`);
-    await queryRunner.query(`ALTER TYPE "public"."message_component_enum_old" RENAME TO "message_component_enum"`);
-    await queryRunner.query(
-      `CREATE TYPE "public"."message_severity_enum_old" AS ENUM('EMERGENCY', 'ALERT', 'CRITICAL', 'ERROR', 'WARNING', 'NOTICE', 'INFORMATIONAL', 'DEBUG')`
-    );
-    await queryRunner.query(
-      `ALTER TABLE "message" ALTER COLUMN "severity" TYPE "public"."message_severity_enum_old" USING "severity"::"text"::"public"."message_severity_enum_old"`
-    );
+    await queryRunner.query(`DROP TABLE "message"`);
+
     await queryRunner.query(`DROP TYPE "public"."message_severity_enum"`);
-    await queryRunner.query(`ALTER TYPE "public"."message_severity_enum_old" RENAME TO "message_severity_enum"`);
+    await queryRunner.query(`DROP TYPE "public"."message_component_enum"`);
   }
 }
