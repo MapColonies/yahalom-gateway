33a5ce1b389ab32b02ff620447d8dbaf0ba7c0ed
liran <lira.tulchin@gmail.com>
2025-10-20 13:21:47 +0300

    test: update mock instances

--- src/types/@map-colonies__openapi-helpers.d.ts
+++ src/types/@map-colonies__openapi-helpers.d.ts
@@ -1,11 +1,11 @@
 declare module '@map-colonies/openapi-helpers' {
-  export function createRequestSender(config?: any): any;
+  export function createRequestSender(config?: unknown): unknown;
 
   export class RequestSender {
-    constructor(config?: any);
-    get(...args: any[]): Promise<any>;
-    post(...args: any[]): Promise<any>;
-    patch(...args: any[]): Promise<any>;
-    delete(...args: any[]): Promise<any>;
+    public constructor(config?: unknown);
+    public get(...args: unknown[]): Promise<unknown>;
+    public post(...args: unknown[]): Promise<unknown>;
+    public patch(...args: unknown[]): Promise<unknown>;
+    public delete(...args: unknown[]): Promise<unknown>;
   }
 }
