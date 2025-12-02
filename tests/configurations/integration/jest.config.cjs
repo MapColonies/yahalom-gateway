120805af4a05beb2a453a64c06ea17632385202a
TULCHINSKI LIRAN <lira.tulchin@gmail.com>
2025-12-02 13:57:44 +0200

    test: extending integration test timeout

--- tests/configurations/integration/jest.config.cjs
+++ tests/configurations/integration/jest.config.cjs
@@ -1,4 +1,3 @@
-jest.setTimeout(60000); // 60 seconds
 const { pathsToModuleNameMapper } = require('ts-jest');
 const { compilerOptions } = require('../../../tsconfig.json');
 
@@ -7,7 +6,7 @@ module.exports = {
   transform: {
     '^.+\\.ts$': ['@swc/jest'],
   },
-  testTimeout: 30000,
+  testTimeout: 60000,
   coverageReporters: ['text', 'html'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
   collectCoverage: true,
