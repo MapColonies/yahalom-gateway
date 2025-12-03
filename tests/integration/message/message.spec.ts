854548053f1b89db2b91d39908f5f405ee448b6a
TULCHINSKI LIRAN <lira.tulchin@gmail.com>
2025-12-03 14:11:16 +0200

    test: fixing tests and adding migration file for creating message_tests table

--- tests/integration/message/message.spec.ts
+++ tests/integration/message/message.spec.ts
@@ -23,7 +23,7 @@ let requestSender: RequestSender<paths, operations>;
 let dependencyContainer: DependencyContainer;
 
 beforeAll(async () => {
-  jest.setTimeout(60000);
+  jest.setTimeout(80000); // 60s
   await initConfig(true);
 
   dependencyContainer = await registerExternalValues({ useChild: true });
