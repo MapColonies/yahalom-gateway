3c369357614747127ddc16a05bfa73a0690d7dd4
TULCHINSKI LIRAN <lira.tulchin@gmail.com>
2025-12-09 09:36:10 +0200

    refactor: fix

--- src/containerConfig.ts
+++ src/containerConfig.ts
@@ -19,8 +19,6 @@ export interface RegisterOptions {
 
 export const registerExternalValues = async (options?: RegisterOptions): Promise<DependencyContainer> => {
   const configInstance = getConfig();
-  type LoggerConfig = Record<string, unknown>;
-
   const loggerConfig = configInstance.get('telemetry.logger');
 
   const logger = jsLogger({ ...loggerConfig, prettyPrint: loggerConfig.prettyPrint, mixin: getOtelMixin() });
